9e62d1228877a79a88424c74895691e8898560d9
rootspyro <root.spyro@gmail.com>
2025-11-12 22:50:29 -0400

    updating colorscheme

--- after/plugin/colorescheme.lua
+++ after/plugin/colorescheme.lua
@@ -1,6 +1,6 @@
 -- Default options:
 require("gruvbox").setup({
-  terminal_colors = true, -- add neovim terminal colors
+  terminal_colors = false, -- add neovim terminal colors
   undercurl = true,
   underline = true,
   bold = true,
@@ -17,7 +17,7 @@ require("gruvbox").setup({
   invert_tabline = false,
   invert_intend_guides = false,
   inverse = true, -- invert background for search, diffs, statuslines and errors
-  contrast = "hard", -- can be "hard", "soft" or empty string
+  contrast = "", -- can be "hard", "soft" or empty string
   palette_overrides = {},
   overrides = {},
   dim_inactive = false,
